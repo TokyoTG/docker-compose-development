c5f74191c2df4d821f25b49b40258dc98b7e5c3b
tokyotg <tboynonilekan@gmail.com>
2022-02-08 16:00:50 +0100

    change storage folder permission

--- composer-install.sh
+++ composer-install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 docker exec -it app.backend composer install
 docker exec -it app.frontend composer install
+docker exec -it app.frontend chmod -R 777 /var/www/storage
+docker exec -it app.backend chmod -R 777 /var/www/storage
