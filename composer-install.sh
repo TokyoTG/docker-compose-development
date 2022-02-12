56d4d648dfbc7751b206a1c68fd748a14aedc719
tokoytg <tboynonilekan@gmail.com>
2022-02-12 12:15:53 +0100

    setup fix

--- composer-install.sh
+++ composer-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker exec -it app.backend composer install
+docker exec -it app.backend php artisan migrate
 docker exec -it app.frontend composer install
 docker exec -it app.frontend chmod -R 777 /var/www/storage
 docker exec -it app.backend chmod -R 777 /var/www/storage
